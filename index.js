14cbfc02307213eee09ebdead02790c89261d260
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-02-02 16:49:25 -0800

    hola mundo

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 /* eslint-disable no-console */
-console.log("hola mundo")
+const express = require ("express")
+const app =  express()
+const port = 3000
+
+app.get("/", (req, res)=>{
+  res.send("Hola mundo")
+})
+
+app.listen(port, () =>{
+  console.log("mi puerto es el " +  port)
+})
